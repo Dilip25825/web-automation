a2e3a1701a53cd7e103601b7ae888f66a6003f5e
Dilip Delwash <ddelwash8@gmail.com>
2026-07-10 17:03:07 +0530

    Udate Setting.py

--- software_admin/settings.py
+++ software_admin/settings.py
@@ -21,8 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.getenv('SECRET_KEY')
-SECRET_KEY = 'django-insecure--vzbo=8+_@h)2#f48moitcex!u9fao^jqhx7k9f5i9i=7#kr-m'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -79,28 +78,19 @@ TEMPLATES = [
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': os.getenv('DB_NAME'),
-#         'USER': os.getenv('DB_USER'),
-#         'PASSWORD': os.getenv('DB_PASSWORD'),
-#         'HOST': os.getenv('DB_HOST'),
-#         'PORT': os.getenv('DB_PORT'),
-#     }
-# }
-
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'bgnok6hzdycwut0u1agt',      # Aapke MySQL database ka naam
-        'USER': 'ufeyotlcp800puxj',      # Database username
-        'PASSWORD': 'N9mBUe4hIMpavg3W0MU9',  # Database password
-        'HOST': 'bgnok6hzdycwut0u1agt-mysql.services.clever-cloud.com',      # Server IP ya localhost
-        'PORT': '20519',                    # Default MySQL port
-        'CONN_MAX_AGE': 900,
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
+}
+
+
+
 }
 
 # Password validation
