942f4c731bd50d3e00e2f721aea01bdc4bd0bac9
Dilip Delwash <ddelwash8@gmail.com>
2026-07-14 17:57:04 +0530

    Update user-facing product branding

--- software_admin/urls.py
+++ software_admin/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import include, path
 
-admin.site.site_header = 'Software Admin'
-admin.site.site_title = 'Software Admin'
-admin.site.index_title = 'Software Admin Administration'
+admin.site.site_header = 'Web Automation with Excel'
+admin.site.site_title = 'Web Automation with Excel'
+admin.site.index_title = 'Web Automation with Excel Administration'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
