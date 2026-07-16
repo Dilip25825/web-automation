bbd873975919f222f7f1e14dafbb33e827e5f10e
Dilip Delwash <ddelwash8@gmail.com>
2026-07-17 00:24:34 +0530

    Update Auto Sleep Disable

--- software_admin/urls.py
+++ software_admin/urls.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
 from django.urls import include, path
-
+from django.http import HttpResponse
 admin.site.site_header = 'Web Automation with Excel'
 admin.site.site_title = 'Web Automation with Excel'
 admin.site.index_title = 'Web Automation with Excel Administration'
 
+
+def health_check(request):
+    return HttpResponse("OK")
+
 urlpatterns = [
+    path("health/", health_check),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('licensing/', include('licensing.urls')),
